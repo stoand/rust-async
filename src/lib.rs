072f25abef2279a0ccbbd81dd64d4824c07ffb6f
Andreas Stocker <shadowstep7@gmail.com>
2016-02-06 18:53:44 +0200

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,39 @@
+//! Automatically generates callbacks under the hood
+//! allowing for async-await style asynchronous programming
+//!
+//! ```rust
+//! #[async]
+//! fn get_user_id() -> Future<User> {
+//!		let user = await!(db.query("SELECT .."));
+//!		user.id
+//! }
+//!
+//! #[async]
+//! fn print_id() {
+//!		println!("user id: {}", await!(get_user_id()));
+//! }
+//! 
+//! ```
+
 #![feature(quote, plugin_registrar, rustc_private, plugin, custom_attribute)]
 #![crate_type = "dylib"]
 
 extern crate syntax;
 extern crate rustc_plugin;
 
+pub mod future;
+
 use rustc_plugin::Registry;
-use syntax::ext::base::{Annotatable, ExtCtxt, SyntaxExtension};
-use syntax::codemap::Span;
-use syntax::parse::token;
-use std::boxed::Box;
-use syntax::feature_gate::AttributeType;
 use syntax::ast;
-use syntax::ptr::P;
+use syntax::codemap::Span;
+use syntax::ext::base::{Annotatable, ExtCtxt, SyntaxExtension};
 use syntax::ext::build::AstBuilder;
+use syntax::ext::quote::rt::ToTokens;
+use syntax::parse::token;
+use syntax::ptr::P;
+
 use std::vec::Vec;
+use std::boxed::Box;
 
 #[plugin_registrar]
 pub fn registrar(reg: &mut Registry) {
@@ -30,19 +50,41 @@ fn async_attribute(cx: &mut ExtCtxt,
     // The function item
     let item = annotable.clone().expect_item();
 
+    // We cannot simply modify the function item
+    // the item, and several of its substructures are wrapped in syntax pointers (syntax::ptr::P)
+    // structs wrapped in these pointers need to be recreated by the AstBuilder
     if let ast::Item_::ItemFn(dec, unsafety, constness, abi, generics, block) = item.node.clone() {
-        let block = cx.block(block.span, generate_callbacks(block.stmts.clone()), block.expr.clone());
+        let block = convert_block(cx, block);
+        let item_fn = ast::Item_::ItemFn(dec, unsafety, constness, abi, generics, block);
 
         Annotatable::Item(cx.item(item.span.clone(),
                                   item.ident.clone(),
                                   item.attrs.clone(),
-                                  ast::Item_::ItemFn(dec, unsafety, constness, abi, generics, block)))
+                                  item_fn))
     } else {
         cx.span_err(span, "The async annotation only works on functions.");
         annotable
     }
 }
 
-fn generate_callbacks(stmts: Vec<P<ast::Stmt>>) -> Vec<P<ast::Stmt>> {
-    vec![]
+fn convert_block(cx: &ExtCtxt, block: P<ast::Block>) -> P<ast::Block> {
+    let stmts = replace_faux_macro_await(cx, block.stmts.clone());
+
+    let expr = quote_expr!(cx,
+                           Box::new(move |callback: &Fn(i32)| {
+                           		$stmts
+                               callback(1);
+                           })
+    );
+
+    cx.block_expr(expr)
+}
+
+fn replace_faux_macro_await(cx: &ExtCtxt, stmts: Vec<P<ast::Stmt>>) -> Vec<P<ast::Stmt>> {
+	 for stmt in stmts.clone() {
+//		 if let ast::Stmt_::StmtDecl(dec, _) = stmt.node.clone() {
+//
+//		 }
+	 }
+    stmts
 }
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![feature(plugin, custom_attribute)]
+#![plugin(async)]
+
+// Run the following command to view expanded code
+// cargo rustc --bin async_tests -- --pretty=expanded -Z unstable-options -o target/debug/main.expanded.rs
+
+mod future;
+
+use future::Future;
+
+static mut race_i : i32 = 0;
+
+// Create a race condition to make sure the code in the async
+// function executes last
+#[test]
+fn async_works() {
+	unsafe { foo(); }
+	unsafe { race_i = 1 };
+}
+
+#[async]
+unsafe fn foo() -> Future<i32> {
+	assert_eq!(1, race_i);
+	race_i = 2;
+}
