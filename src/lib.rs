b0c46a9be302541bd7f1be29d33ac3ac2748ceb0
Andreas Stocker <shadowstep7@gmail.com>
2016-02-09 11:24:23 +0200

    Make callback part of function params

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,18 @@
 //! ```rust
 //! #[async]
 //! fn get_user_id() -> Future<User> {
-//!		let user = await!(db.query("SELECT .."));
-//!		user.id
+//! 		let user = await!(db.query("SELECT .."));
+//! 		user.id
 //! }
 //!
 //! #[async]
 //! fn print_id() {
-//!		println!("user id: {}", await!(get_user_id()));
+//! 		println!("user id: {}", await!(get_user_id()));
 //! }
-//! 
+//!
 //! ```
 
-#![feature(quote, plugin_registrar, rustc_private, plugin, custom_attribute)]
+#![feature(quote, plugin_registrar, rustc_private, plugin, custom_attribute, advanced_slice_patterns, slice_patterns)]
 #![crate_type = "dylib"]
 
 extern crate syntax;
@@ -31,8 +31,8 @@ use syntax::ext::build::AstBuilder;
 use syntax::ext::quote::rt::ToTokens;
 use syntax::parse::token;
 use syntax::ptr::P;
-
 use std::vec::Vec;
+
 use std::boxed::Box;
 
 #[plugin_registrar]
@@ -54,9 +54,22 @@ fn async_attribute(cx: &mut ExtCtxt,
     // the item, and several of its substructures are wrapped in syntax pointers (syntax::ptr::P)
     // structs wrapped in these pointers need to be recreated by the AstBuilder
     if let ast::Item_::ItemFn(dec, unsafety, constness, abi, generics, block) = item.node.clone() {
-        let block = convert_block(cx, block);
+        // Recursively modify statements
+        let stmts = handle_statements(cx, block.stmts.clone());
+        let block = cx.block(block.span, stmts, block.expr.clone());
+
+        let ty = match dec.output.clone() {
+            ast::FunctionRetTy::Return(ty) => ty,
+            _ => quote_ty!(cx, ()),
+        };
+
+        let mut inputs = dec.inputs.clone();
+        inputs.push(quote_arg!(cx, cb: &FnOnce($ty)));
+        let dec = cx.fn_decl(inputs, quote_ty!(cx, ()));
+
         let item_fn = ast::Item_::ItemFn(dec, unsafety, constness, abi, generics, block);
 
+        // cx.span_err(span, "read printed stuff");
         Annotatable::Item(cx.item(item.span.clone(),
                                   item.ident.clone(),
                                   item.attrs.clone(),
@@ -67,24 +80,67 @@ fn async_attribute(cx: &mut ExtCtxt,
     }
 }
 
-fn convert_block(cx: &ExtCtxt, block: P<ast::Block>) -> P<ast::Block> {
-    let stmts = replace_faux_macro_await(cx, block.stmts.clone());
+/// Convert statements that contain the await! macro into callbacks
+fn handle_statements(cx: &ExtCtxt, stmts: Vec<P<ast::Stmt>>) -> Vec<P<ast::Stmt>> {
+    if let Some((stmt, stmts_below)) = stmts.split_first() {
+        // We only check for await in declaration statments
+        // TODO check for await in other places
+        if let ast::Stmt_::StmtDecl(_, _) = stmt.node.clone() {
+            // If this is the last async statement we invoke the Future's callback
+            let stmts_inside_cb = if stmts_below.is_empty() {
+                handle_statements(cx, stmts_below.to_vec())
+            } else {
+                vec![
+//                quote_stmt!(cx,
+//                                 cb({
+//                                     1234
+//                                 }))
+//                         .unwrap()
+                         ]
+            };
 
-    let expr = quote_expr!(cx,
-                           Box::new(move |callback: &Fn(i32)| {
-                           		$stmts
-                               callback(1);
-                           })
-    );
+            vec![quote_stmt!(cx, {
+				$stmt
+				if (true) {
+					$stmts_inside_cb
+				}
+            	})
+                     .unwrap()]
+        } else {
+            // An expression statement may contain statements within itself depending
+            // on the expression type
+            let stmt: P<ast::Stmt> = match stmt.node.clone() {
+                ast::Stmt_::StmtExpr(expr, _) => cx.stmt_expr(handle_expression(cx, expr)),
+                ast::Stmt_::StmtSemi(expr, _) => cx.stmt_expr(handle_expression(cx, expr)),
+                _ => stmt.clone(),
+            };
 
-    cx.block_expr(expr)
+            // No await macro found, carry on normally and look for more await! macros
+            match stmts_below.is_empty() {
+                false => {
+                    let mut stmts = Vec::new();
+                    stmts.push(stmt.clone());
+                    stmts.extend(handle_statements(cx, stmts_below.to_vec()));
+
+                    stmts
+                }
+                true => vec![quote_stmt!(cx, cb({$stmt})).unwrap()],
+            }
+        }
+    } else {
+        vec![]
+    }
 }
 
-fn replace_faux_macro_await(cx: &ExtCtxt, stmts: Vec<P<ast::Stmt>>) -> Vec<P<ast::Stmt>> {
-	 for stmt in stmts.clone() {
-//		 if let ast::Stmt_::StmtDecl(dec, _) = stmt.node.clone() {
-//
-//		 }
-	 }
-    stmts
+fn handle_expression(cx: &ExtCtxt, expr: P<ast::Expr>) -> P<ast::Expr> {
+    use syntax::ast::Expr_::*;
+
+    let node = match expr.node.clone() {
+        // 		ExprWhile(expr, block, indent) => {
+        // 			ExprWhile(expr, cx.block(), indent);
+        // 		},
+        n @ _ => n.clone(),
+    };
+
+    cx.expr(expr.span, node)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,49 @@
 #![plugin(async)]
 
 // Run the following command to view expanded code
-// cargo rustc --bin async_tests -- --pretty=expanded -Z unstable-options -o target/debug/main.expanded.rs
+// cargo rustc --bin async_tests -- --pretty=expanded
+// -Z unstable-options -o target/debug/main.expanded.rs
 
-mod future;
+use std::vec::Vec;
 
-use future::Future;
-
-static mut race_i : i32 = 0;
+static mut race_i: i32 = 0;
 
 // Create a race condition to make sure the code in the async
 // function executes last
-#[test]
-fn async_works() {
-	unsafe { foo(); }
-	unsafe { race_i = 1 };
-}
+// #[test]
+// fn async_works() {
+// 	unsafe { foo(); }
+// 	unsafe { race_i = 1 };
+// }
+
+// #[async]
+// unsafe fn foo() -> Future<i32> {
+// 	assert_eq!(1, race_i);
+// 	race_i = 2;
+// }
+
+// let (user1, user2) = await!(db.get_user(1), b.get_user(2));
+
+
+//trait Future {
+//	fn parallel(callbacks: Vec<&Self>);
+//}
 
 #[async]
-unsafe fn foo() -> Future<i32> {
-	assert_eq!(1, race_i);
-	race_i = 2;
+fn foo() -> i32 {
+    let mut a = 1;
+    a = 2;
+
+	while false {
+		let d = 1;
+		let g = 1;
+	}
+    let c = 1;
+	a = 5;
 }
+
+// fn foo() {
+// 	let user1 = db.get_user(1);
+// 	let user2 = db.get_user(2);
+// 	let user = db.get_user();
+// }
